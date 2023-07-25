bfc7de0bd1cfd30cf4e84f25f35e765723392cf5
Vinicius Aquino Dias <vdeaquinodias@gmail.com>
2023-07-25 13:06:13 -0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿start:
+
+Console.Write("Multiplication Table\n");
+Console.Write("What's the number you want to put in table? ");
+int n1 = Convert.ToInt16(Console.ReadLine());
+
+for(int i = 0;i <= 10; i++)
+{
+    Console.WriteLine($"{n1} X {i} = {n1*i}");
+}
+Console.ReadKey();
+Console.Clear();
+goto start;
